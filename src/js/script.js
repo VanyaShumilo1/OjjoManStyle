29c3e04be9d80d40a4c0a5f313921811612af8df
Vanya Shumilo <ivan.shumilo@gmail.com>
2022-04-05 22:49:22 +0300

    JS Jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,31 @@
-const tabsBtn = document.querySelectorAll(".events__menu-item");
-const tabsItems = document.querySelectorAll(".events__images-item");
+// const tabsBtn = document.querySelectorAll(".events__menu-item");
+// const tabsItems = document.querySelectorAll(".events__images-item");
 
 
-tabsBtn.forEach(function(item) {
-    item.addEventListener("click", function(){
+// tabsBtn.forEach(function(item) {
+//     item.addEventListener("click", function(){
 
-        let tabId = item.getAttribute("data-tab");
-        let currentTab = document.querySelector(tabId);
+//         let tabId = item.getAttribute("data-tab");
+//         let currentTab = document.querySelector(tabId);
 
-        tabsBtn.forEach(function(item){
-            item.classList.remove("menu_active");
-        });
+//         tabsBtn.forEach(function(item){
+//             item.classList.remove("menu_active");
+//         });
 
-        tabsItems.forEach(function(item){
-            item.classList.remove("active");
-        });
+//         tabsItems.forEach(function(item){
+//             item.classList.remove("active");
+//         });
 
-        item.classList.add("menu_active");
-        currentTab.classList.add("active");
+//         item.classList.add("menu_active");
+//         currentTab.classList.add("active");
 
-    });
+//     });
+// });
+
+
+$('div.events__menu').on('click', '.events__menu-item:not(.menu_active)', function() {
+    $(this)
+        .addClass('menu_active').siblings().removeClass('menu_active')
+        .closest('div.container').find('div.events__images').removeClass('events__images-active').eq($(this).index()).addClass('events__images-active');
 });
+
